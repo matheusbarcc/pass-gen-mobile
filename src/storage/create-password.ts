d7903c177b6a7f92ea484f07a39ac6aab4973d38
matheusbarcc <matheusbarcon@gmail.com>
2025-03-31 19:02:15 -0300

    feat: add function to add a new password to the async storage

---
+++ src/storage/create-password.ts
@@ -0,0 +1,32 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { v4 as uuidv4 } from 'uuid'
+import { PASSWORD_COLLETION } from './storageConfig'
+import { getAllPasswords } from './get-all-passwords'
+
+export async function createPassword() {
+  try {
+    const storedPasswords = await getAllPasswords()
+    const newPassword = uuidv4().slice(1, 8)
+
+
+    const today = new Date('2025-04-02').toLocaleDateString('pt-BR').replaceAll('/', '.')
+
+    const dayAlreadyExists = storedPasswords.find(daylist => daylist.date === today)
+
+    if (dayAlreadyExists) {
+      storedPasswords.map(daylist => {
+        if (daylist.date === today) {
+          daylist.passwords.push(newPassword)
+        }
+      })
+    } else {
+      storedPasswords.push({ date: today, passwords: [newPassword] })
+    }
+
+    await AsyncStorage.setItem(PASSWORD_COLLETION, JSON.stringify(storedPasswords))
+
+    return newPassword
+  } catch (error) {
+    throw error
+  }
+}
